1e4bdb4363be93c0b846eaa4efca5629939c4a31
danhonoriio <dan.honoriio@gmail.com>
2026-04-05 20:22:07 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,13 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-
+import BusinessClientDetails from "./pages/BusinessClientDetails";
 
 function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path="/business-client-details" component={BusinessClientDetails} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
